ce10cab071cc987853924249da692e6e405488fb
tienvvpowergate <tien.vuvan@powergatesoftware.com>
2019-02-07 01:41:15 +0700

    update

--- AdvancedNodeJS/services/cache.js
+++ AdvancedNodeJS/services/cache.js
@@ -4,14 +4,24 @@ const util = require('util');
 
 const redisUrl = 'redis://127.0.0.1:6379';
 const client = redis.createClient(redisUrl);
-client.get = util.promisify(client.get);
+client.hget = util.promisify(client.hget);
 const exec = mongoose.Query.prototype.exec;
 //const find = mongoose.Query.prototype.find;
+
+mongoose.Query.prototype.cache = function(options = {}) {
+  this.useCache = true;
+  this.hashKey = JSON.stringify(options.key || '');
+  return this;
+};
 
 // we are using the function keyword and not an arrow function
 // because an arrow function tryies to mess around with the value of
 // this inside the function.
-mongoose.Query.prototype.exec = async function () {
+mongoose.Query.prototype.exec = async function() {
+  if (!this.useCache) {
+    return exec.apply(this, arguments);
+  }
+
   // so inside this function if we were to write out `this` it should
   // reference the query that is being produced
   console.log('Im about to run a query');
@@ -25,30 +35,29 @@ mongoose.Query.prototype.exec = async function () {
 
   // console.log(this.getQuery()); // --> { _user: '5c45e9851bb70230a4b7d201' }
   // console.log(this.mongooseCollection.name); // --> blogs
-  const key = JSON.stringify(Object.assign({}, this.getQuery(), {
-    collection: this.mongooseCollection.name
-  }));
+  const key = JSON.stringify(
+    Object.assign({}, this.getQuery(), {
+      collection: this.mongooseCollection.name
+    })
+  );
 
   // console.log(key);
-  console.log(1)
   // See if we have avalue for 'key' in redis
-  const cacheValue = await client.get(key);
+  const cacheValue = await client.hget(this.hashKey, key);
 
   // if we do, return that
   if (cacheValue) {
-    console.log(2)
     console.log('cacheValue', cacheValue);
     const doc = JSON.parse(cacheValue);
 
-    return Array.isArray(doc) ? doc.map(d => new this.model(d)) : new this.model(doc);
+    return Array.isArray(doc)
+      ? doc.map(d => new this.model(d))
+      : new this.model(doc);
   }
-  console.log(3)
   // otherwise, issue the query and store the result
   const result = await exec.apply(this, arguments);
-  console.log(4)
   // console.log('result', result.validate);
-  client.set(key, JSON.stringify(result));
-  console.log(5)
+  client.hset(this.hashKey, key, JSON.stringify(result));
   return result;
 
   // return mongoose.Query.prototype.exec.apply(this, arguments);
@@ -59,3 +68,9 @@ mongoose.Query.prototype.exec = async function () {
 
 //   return find.apply(this, arguments);
 // };
+
+module.exports = {
+  clearHash(hashKey) {
+    client.del(JSON.stringify(hashKey));
+  }
+};
