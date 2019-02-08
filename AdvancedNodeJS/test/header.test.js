37af74e059e7d7ea7e941d035b01e25616c41337
tienvvpowergate <tien.vuvan@powergatesoftware.com>
2019-02-08 23:09:35 +0700

    update

--- AdvancedNodeJS/test/header.test.js
+++ AdvancedNodeJS/test/header.test.js
@@ -5,18 +5,20 @@ test('Adds two numbers', () => {
   expect(sum).toEqual(3);
 });
 
-test('test', async () => {
-  const browser = await puppeteer.launch({
-    headless: true, //this will make browser opened without some graphic
-    args: ['--no-sandbox']
+describe('When logged in', async () => {
+  test('test', async () => {
+    const browser = await puppeteer.launch({
+      headless: true, //this will make browser opened without some graphic
+      args: ['--no-sandbox']
+    });
+    const page = await browser.newPage();
+
+    // await page.goto('localhost:3000'); for local
+    await page.goto('http://localhost:3000'); // for travis
+
+    const text = await page.$eval('a.brand-logo', el => el.innerHTML);
+
+    expect(text).toEqual('Blogster');
+    await page.close();
   });
-  const page = await browser.newPage();
-
-  // await page.goto('localhost:3000'); for local
-  await page.goto('http://localhost:3000'); // for travis
-
-  const text = await page.$eval('a.brand-logo', el => el.innerHTML);
-
-  expect(text).toEqual('Blogster');
-  await page.close();
 });
