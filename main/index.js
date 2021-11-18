f2663cd1867143ea5fb5087aa2e0407df0493bbd
tienvv1234 <tienvv1234@gmail.com>
2021-11-18 13:59:43 +0700

    update

--- main/index.js
+++ main/index.js
@@ -1,16 +1,75 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const crypto = require('crypto');
+const crypto = require("crypto");
 
-app.get('/', (req, res) => {
-  // doWork(5000); // this will blocking the event loop about 5s
-  crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
-    res.send('Hi there');
+const blocking = async () => {
+  const start = Date.now();
+  while (new Date() - start < 5000) {
+    console.log("111111111111111111111111");
+  }
+};
+
+const blocking2 = async () => {
+  return new Promise((resolve, reject) => {
+    const start = Date.now();
+    let sum = 0; //
+    let i = 0;
+    while (i < 5000000000) {
+      sum += i;
+      i += 1;
+    }
+    console.log("sum", sum);
+    resolve(sum);
   });
+};
+
+const blocking3 = async () => {
+  const start = Date.now();
+  let sum = 0; //
+  let i = 0;
+  while (i < 5000000000) {
+    sum += i;
+    i += 1;
+  }
+  console.log("sum", sum);
+  return sum;
+};
+
+app.get("/", async (req, res) => {
+  // doWork(5000); // this will blocking the event loop about 5s
+  // crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
+  //   res.send('Hi there');
+  // });
+  // await blocking();
+
+  blocking2()
+    .then((v) => {
+      console.log(11, v);
+    })
+    .then(() => {
+      blocking3().then((v) => {
+        console.log(22, v);
+      }).catch(err => console.log(err))
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+  console.log(33);
+
+  // blocking3()
+  // .then(v => {
+  //   console.log(22, v);
+  // })
+  // .catch(err => {
+  //   console.log(err);
+  // })
+
+  res.send("hi there111111111");
+  console.log("end");
 });
 
-app.get('/fast', (req, res) => {
-  res.send('this was fast!');
+app.get("/fast", (req, res) => {
+  res.send("this was fast!");
 });
 
 app.listen(3000);
